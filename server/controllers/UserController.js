41a3a9aece2f0d91e86e223050e0c3faf4ba766f
ericksonmanurung <erickson.manurung23@gmail.com>
2021-04-27 21:40:48 +0700

    commit3

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,18 +1,32 @@
 const {User} = require('../models')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 class UserController{
     static register(req,res,next){
         const {email,password} = req.body
         User.create({email,password})
         .then((user) => {
-            res.status
+            res.status(201).json({success:true, data:user})
         }).catch((err) => {
             next(err)
         });
     }
 
-    static login(req,res){
-        res.send('login')
+    static login(req,res,next){
+        const {email,password} = req.body
+        User.findOne({where:{email}})
+        .then((user) => {
+            // console.log(user)
+            if(user && bcrypt.compareSync(password, user.password)){
+                const access_token = jwt.sign({ id:user.id, email:user.email  },process.env.JWT_SECREATE);
+                res.status(200).json({success:true, access_token})
+            }else{
+                throw{name:'LOGIN_ERROR'}
+            }
+        }).catch((err) => {
+            next(err)
+        });
     }
 
 
